156c14b62c1028da4f9eae24d95d2898d22803ba
KWJACK <dndudwp12@gmail.com>
2017-05-02 23:38:23 +0900

    5/2일 작업 내용 - write/read 스레드로 통신 / 네이버지도 액티비티에서 fragment식으로 띄우기 / 전화 수신시 번호 띄우기- 전화 수신은 안되나

--- app/src/main/java/com/example/coms7/threadmessage/MainActivity.java
+++ app/src/main/java/com/example/coms7/threadmessage/MainActivity.java
@@ -4,13 +4,24 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.session.MediaSessionManager;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Telephony;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
     int mainValue = 0;
@@ -21,10 +32,20 @@ public class MainActivity extends AppCompatActivity {
     String [] alphabet={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
     Button [] myBtn= new Button[3];
     Intent intentSubActivity;
+    PhoneStateCheckListener phoneCheckListener;
+    private Context mCtx;
+    private MediaSessionManager mSessionManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        phoneCheckListener = new PhoneStateCheckListener(this);
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+        telephonyManager.listen(phoneCheckListener, PhoneStateListener.LISTEN_CALL_STATE);
 
         mainText = (TextView)findViewById(R.id.mainvalue);
         backText = (TextView)findViewById(R.id.backvalue);
@@ -36,13 +57,14 @@ public class MainActivity extends AppCompatActivity {
         myBtn[0].setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                intentSubActivity = new Intent(MainActivity.this, MapActivity.class);
+                intentSubActivity = new Intent(MainActivity.this, MapActivity.class); // 예제로 네이버 지도를 다른 액티비티에서 띄워보기
                 startActivityForResult(intentSubActivity, 2);
             }
         });
         myBtn[1].setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+
 
             }
         });
@@ -64,10 +86,10 @@ public class MainActivity extends AppCompatActivity {
         thread_read.start();
     }
 
-    public void mOnClick(View v){
-        mainValue++;
-        mainText.setText("MainValue:" + mainValue);
-    }
+//    public void mOnClick(View v){
+//        mainValue++;
+//        mainText.setText("MainValue:" + mainValue);
+//    }
 
     class WriteThread extends Thread{   //ToDO 나중에 UDP 입력을 받고, 여기서 액션을 걸러내야함
         @Override
@@ -94,6 +116,66 @@ public class MainActivity extends AppCompatActivity {
             } // end while
         } // end run()
     } // end class BackThread
+
+
+    public class PhoneStateCheckListener extends PhoneStateListener {   //전화오면 번호띄우는거까지댐
+        MainActivity mainActivity;
+        PhoneStateCheckListener(MainActivity _main){
+            mainActivity = _main;
+        }
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            if (state == TelephonyManager.CALL_STATE_IDLE) {
+                Toast.makeText(mainActivity,"STATE_IDLE : Incoming number "
+                        + incomingNumber,Toast.LENGTH_SHORT).show();
+            } else if (state == TelephonyManager.CALL_STATE_RINGING) {
+                Toast.makeText(mainActivity,"STATE_RINGING : Incoming number "
+                        + incomingNumber,Toast.LENGTH_SHORT).show();//수신 부분 입니다.
+
+//                interruptCall interrupt = new interruptCall();    //ToDo 전화오면 수신.. 안되무
+//                interrupt.start();
+
+            } else if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
+                Toast.makeText(mainActivity,"STATE_OFFHOOK : Incoming number "
+                        + incomingNumber,Toast.LENGTH_SHORT).show();
+            }
+        }
+
+//        public class interruptCall extends Thread{
+//            public void run(){
+//                try{
+//                    sleep(1000);
+//                }catch(InterruptedException e){
+//                    e.printStackTrace();
+//                }
+//                try
+//                {
+////                    acceptCall();
+////                    Intent new_intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+////                    new_intent.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
+////                    mainActivity.sendOrderedBroadcast(new_intent, null);
+////                    new_intent = null;
+//                } catch (Exception e)
+//                {
+//                    e.printStackTrace();
+//                }
+//
+//                try
+//                {
+//                    Intent buttonUp = new Intent(Intent.ACTION_MEDIA_BUTTON);
+//                    buttonUp.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HEADSETHOOK));
+//                    mainActivity.sendOrderedBroadcast(buttonUp, null);
+//                    buttonUp = null;
+//                } catch (Exception e)
+//                {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+    }
+
+
 
     class ReadThread extends Thread{    //ToDO WriteThread에서 액션을 주면 핸들러에서 작업. 버튼을 트리거시킴
         @Override
@@ -138,5 +220,38 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+
+//    private Method getMethodDispatchMediaKey() { http://sherl.tistory.com/43
+//        mSessionManager = (MediaSessionManager) mCtx.getSystemService(Context.MEDIA_SESSION_SERVICE);
+//        Method m = null;
+//        try {
+//            m = Class.forName(mSessionManager.getClass().getName()).getDeclaredMethod("dispatchMediaKeyEvent", KeyEvent.class);
+//            m.setAccessible(true);
+//        } catch (IllegalArgumentException e) {
+//            e.printStackTrace();
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        return m;
+//    }
+//
+//    public void acceptCall(){
+//        try {
+//            getMethodDispatchMediaKey().invoke(mSessionManager, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
+//            getMethodDispatchMediaKey().invoke(mSessionManager, new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HEADSETHOOK));
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (IllegalArgumentException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
 
 }
--- app/src/main/java/com/example/coms7/threadmessage/MapActivity.java
+++ app/src/main/java/com/example/coms7/threadmessage/MapActivity.java
@@ -1,12 +1,14 @@
 package com.example.coms7.threadmessage;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.nhn.android.maps.NMapActivity;
 import com.nhn.android.maps.NMapView;
 
-public class MapActivity extends NMapActivity {
+public class MapActivity extends FragmentActivity {
 
     private NMapView mMapView;// 지도 화면 View
     private final String CLIENT_ID = "mePRJxFXCilrYbvJINuX";// 애플리케이션 클라이언트 아이디 값
@@ -14,13 +16,20 @@ public class MapActivity extends NMapActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mMapView = new NMapView(this);
-        setContentView(mMapView);
-        mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
-        mMapView.setClickable(true);
-        mMapView.setEnabled(true);
-        mMapView.setFocusable(true);
-        mMapView.setFocusableInTouchMode(true);
-        mMapView.requestFocus();
+//        mMapView = new NMapView(this);
+//        setContentView(mMapView);
+//        mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
+//        mMapView.setClickable(true);
+//        mMapView.setEnabled(true);
+//        mMapView.setFocusable(true);
+//        mMapView.setFocusableInTouchMode(true);
+//        mMapView.requestFocus();
+        setContentView(R.layout.activity_map);
+        Fragment1 fragment1 = new Fragment1();
+        fragment1.setArguments(new Bundle());
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentHere, fragment1);
+        fragmentTransaction.commit();
     }
 }
